0b00c46b27101ffc3dab3b560a356271808408cf
Clovis Pressard <pcetudeing@gmail.com>
2024-12-02 11:06:27 +0100

    Adding a git cheat sheet and update version of the code for the question 1 and 2

--- TP1/enseash.c
+++ TP1/enseash.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
+#include <time.h>
 // In this code we print messages then
 // wait for the user to type in order to exit
 // the program
@@ -13,26 +13,42 @@
 //	./enseash 
 //to land the executable.
 
-void enseash(){
 
-	//Variable
+
+/*
+*	Call of the different command of the user to the OS
+*/
+void call_Command(char command[25]){
+	int cr = system( command);
+	if ( cr != 0 ) {
+		fprintf( stderr,"Impossible de lancer la commande : %s\n",command );
+	}
+}
+
+void exit_command(char command[25]){
 	int comparString;
 
-	//
+	comparString=strcmp(command,"exit");
+	if(comparString == 0){
+		exit(0);
+	}	
+}
+
+
+void enseash(){
 	printf("Welcome to ENSEA Tiny SHell\n");
-	printf("Type to exit\n");
-	
+	printf("Type 'exit' to quit\n");	
 	char typed_return[25];
 
 	while(1){
 		printf("enseash %% ");
 		scanf("%s",typed_return);
-		comparString=strcmp(typed_return,"exit");
-		if(comparString == 0){
-			exit(0);
-		}	
+		exit_command(typed_return);
+		call_Command(typed_return);
 	}
 }
+
+
 
 int main(){
 	enseash();

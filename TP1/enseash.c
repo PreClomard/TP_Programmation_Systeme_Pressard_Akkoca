c46ec6e17b792bdc3509469a6f193c0602f99af2
Clovis Pressard <pcetudeing@gmail.com>
2024-12-09 08:20:22 +0100

    Evolution with the use of write and read instead of printf and Scanf. And use exec instead of System

--- TP1/enseash.c
+++ TP1/enseash.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
 // In this code we print messages then
 // wait for the user to type in order to exit
 // the program
@@ -18,33 +21,62 @@
 /*
 *	Call of the different command of the user to the OS
 */
-void call_Command(char command[25]){
-	int cr = system( command);
-	if ( cr != 0 ) {
-		fprintf( stderr,"Impossible de lancer la commande : %s\n",command );
+void call_Command(char input[25]){
+	pid_t pid = fork();
+	if (pid == 0) { // Child process
+		char *args[] = {input, NULL};
+		execvp(args[0], args);
+		const char *error_message = "Command not found\n";
+		write(STDOUT_FILENO, error_message, strlen(error_message));
+		_exit(1);
+	} else if (pid > 0) { // Parent process
+		int status;
+		waitpid(pid, &status, 0);
+	} else {
+		const char *error_message = "Fork failed\n";
+		write(STDOUT_FILENO, error_message, strlen(error_message));
 	}
 }
 
+/*
+*	Exit the micro shell
+*/
 void exit_command(char command[25]){
+	const char *bye_message = "Bye bye...\n";
 	int comparString;
 
 	comparString=strcmp(command,"exit");
 	if(comparString == 0){
+		write(STDOUT_FILENO, bye_message, strlen(bye_message));
 		exit(0);
 	}	
 }
 
 
+/*
+*	Function to create the micro shell
+*/
 void enseash(){
-	printf("Welcome to ENSEA Tiny SHell\n");
-	printf("Type 'exit' to quit\n");	
+	const char *welcome_message = "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\n";
+    const char *prompt = "enseash %% ";
+	const char *exit_shell = "Type 'exit' to quit\n";
+
+	write(STDOUT_FILENO, welcome_message, strlen(welcome_message));
+	write(STDOUT_FILENO, exit_shell , strlen(exit_shell));
+	
+	
 	char typed_return[25];
 
-	while(1){
-		printf("enseash %% ");
-		scanf("%s",typed_return);
-		exit_command(typed_return);
-		call_Command(typed_return);
+	for(int i=0;i<6;i++){
+		
+		write(STDOUT_FILENO, prompt , strlen(prompt ));
+		ssize_t length = read(STDIN_FILENO, typed_return, sizeof(typed_return) - 1);
+		if(length>0){
+			typed_return[length - 1] = '\0';
+			exit_command(typed_return);
+			call_Command(typed_return);
+		}
+		
 	}
 }
 
